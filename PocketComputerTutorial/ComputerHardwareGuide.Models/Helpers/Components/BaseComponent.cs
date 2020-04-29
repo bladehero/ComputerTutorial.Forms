e7927ee790000c959f0e414bd488bd22b39c2a92
au-jourdhui <petitefille0910@gmail.com>
2020-04-29 21:56:00 +0300

    API + Models

---
+++ PocketComputerTutorial/ComputerHardwareGuide.Models/Helpers/Components/BaseComponent.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ComputerHardwareGuide.Models.Components
+{
+    public class BaseComponent : BaseEntity
+    {
+        public BaseComponent()
+        {
+            ComponentPictures = new List<ComponentPicture>();
+        }
+
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public double? Cost { get; set; }
+        public Firm Firm { get; set; }
+        public int? YearOfIssue { get; set; }
+        public string Size { get; set; }
+        public double? Weight { get; set; }
+
+
+        [NotMapped]
+        public List<ComponentPicture> ComponentPictures { get; set; }
+        [NotMapped]
+        public virtual ComponentTypeEnumeration Type { get; }
+    }
+}
---
+++ PocketComputerTutorial/ComputerHardwareGuide.Models/Helpers/Components/ComponentType.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel;
+
+namespace ComputerHardwareGuide.Models.Components
+{
+    public class ComponentType : BaseEntity
+    {
+        public string Name { get; set; }
+        public ComponentTypeEnumeration ComponentTypeEnumeration => (ComponentTypeEnumeration)Id;
+    }
+
+    public enum ComponentTypeEnumeration : int
+    {
+        [Description("CPU")]
+        CPU = 1,
+        [Description("RAM")]
+        RAM = 2,
+        [Description("GPU")]
+        GPU = 3,
+        [Description("Power Unit")]
+        PowerUnit = 4,
+        [Description("Motherboard")]
+        Motherboard = 5,
+        [Description("HDD")]
+        HDD = 6,
+        [Description("SSD")]
+        SSD = 7,
+        [Description("Cooling System")]
+        CoolingSystem = 8,
+        [Description("Case")]
+        Case = 9
+    }
+}
